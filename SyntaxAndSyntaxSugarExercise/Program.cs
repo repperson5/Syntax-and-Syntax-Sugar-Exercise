dd70d23d4f1ca5f679485e9d46bbd52296924ab1
Rhea Epperson <re.epperson2@gmail.com>
2023-01-02 12:53:31 -0500

    finished exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            
+
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var answer = 14;
+            var response = (answer < 9) ? $" {answer} is less than nine" : $" {answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+;        }
     }
 }
